69c9f4bb8ecffc8fbb3d74cc4db6fb8385cbde76
veerprabakar <prabakar.veer@gmail.com>
2017-10-30 10:33:25 +1300

    working version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer, Input, HostListener, HostBinding, OnInit } from '@angular/core';
+import { FileUploader, FileUploaderOptions, FileSelectDirective } from 'ng2-file-upload';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public uploader =  new FileUploader(
+    {
+      url: 'http://localhost:56155/api/UploadFile',
+      method: 'POST',
+      headers: [{ name: 'Access-Control-Allow-Origin', value: 'http://localhost:56155/'}]
+    }
+  );
+  public hasBaseDropZoneOver = false;
+  public hasAnotherDropZoneOver = false;
+
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  public fileOverAnother(e: any): void {
+    this.hasAnotherDropZoneOver = e;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FileDropDirective, FileSelectDirective } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileDropDirective,
+    FileSelectDirective
   ],
   imports: [
     BrowserModule
