689ec830563210b385f3ee14320d3b8a7c88296c
veerprabakar <prabakar.veer@gmail.com>
2017-10-30 16:05:57 +1300

    working 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component, ElementRef, Renderer, Input, HostListener, HostBinding, OnInit } from '@angular/core';
+import { Component, ElementRef, Renderer, Input, HostListener, HostBinding, OnInit, ViewContainerRef } from '@angular/core';
 import { FileUploader, FileUploaderOptions, FileSelectDirective } from 'ng2-file-upload';
+
+import { MatButtonModule } from '@angular/material';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +10,20 @@ import { FileUploader, FileUploaderOptions, FileSelectDirective } from 'ng2-file
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(public toastr: ToastsManager, vcr: ViewContainerRef) {
+    this.toastr.setRootViewContainerRef(vcr);
+  }
+
   title = 'app';
   public uploader =  new FileUploader(
     {
       url: 'http://localhost:56155/api/UploadFile',
-      method: 'POST',
-      headers: [{ name: 'Access-Control-Allow-Origin', value: 'http://localhost:56155/'}]
+      method: 'POST'
+      //,
+      // headers: [{
+      //   name: 'Access-Control-Allow-Origin',
+      //   value: 'http://localhost:56155/'}]
     }
   );
   public hasBaseDropZoneOver = false;
@@ -25,4 +36,10 @@ export class AppComponent {
   public fileOverAnother(e: any): void {
     this.hasAnotherDropZoneOver = e;
   }
+
+  showSuccess() {
+    this.toastr.success('You are awesome!', 'Success!');
+  }
+
+  //this.toastr.sucess('This toast will dismiss in 10 seconds.', null, {toastLife: 10000});
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FileDropDirective, FileSelectDirective } from 'ng2-file-upload';
 
+// Material and animation :
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatProgressBarModule } from '@angular/material';
+
+// Toasty :
+import {ToastModule} from 'ng2-toastr/ng2-toastr';
+import { ToastOptions } from 'ng2-toastr/src/toast-options';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -11,9 +19,14 @@ import { FileDropDirective, FileSelectDirective } from 'ng2-file-upload';
     FileSelectDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    ToastModule.forRoot()
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
